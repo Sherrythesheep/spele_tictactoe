126cc7da0b34d22ac2fa72e1ba07c6f379fba66e
Sherrythesheep <evelinapuga07@gmail.com>
2023-11-23 14:36:17 +0200

    Update tictactoe.py
    23.11.23

--- tictactoe.py
+++ tictactoe.py
@@ -30,13 +30,17 @@ def checkWinner():
     winner=False #noteijs, ja bus neizskirts
     if (btn1["text"]=="X"and btn2["text"]=="X" and btn3["text"]=="X" or btn4["text"]=="X"and btn5["text"]=="X" and btn6["text"]=="X" or btn7["text"]=="X"and btn8["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn4["text"]=="X" and btn7["text"]=="X" or btn2["text"]=="X"and btn5["text"]=="X" and btn9["text"]=="X" or btn1["text"]=="X"and btn5["text"]=="X" and btn9["text"]=="X" or btn7["text"]=="X"and btn5["text"]=="X" and btn3["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X"):
         winner=True
+        disableButtons()
         messagebox.showinfo("TicTacToe","Spēlētājs X ir uzvarētājs!")
     elif (btn1["text"]=="O"and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O"and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O"and btn8["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn4["text"]=="O" and btn7["text"]=="O" or btn2["text"]=="O"and btn5["text"]=="O" and btn9["text"]=="O" or btn1["text"]=="O"and btn5["text"]=="O" and btn9["text"]=="O" or btn7["text"]=="O"and btn5["text"]=="O" and btn3["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O"):
         winner=True
+        disableButtons()
         messagebox.showinfo("TicTacToe", "Spēlētājs O ir uzvarētājs!")
     elif count==9 and winner==False:
-        messagebox.showinfo("TicTacToe","Spēle beidzās ar neizšķirt!")
+        disableButtons()
+        messagebox.showinfo("TicTacToe","Spēle beidzās ar neizšķirtu!")
     return
+
     
 btn1=Button(mansLogs,text="",width=6, height=3,font=("Helvica", 24), command=lambda:btnClick(btn1))
 btn2=Button(mansLogs,text="",width=6, height=3,font=("Helvica", 24), command=lambda:btnClick(btn2))
@@ -48,6 +52,19 @@ btn7=Button(mansLogs,text="",width=6, height=3,font=("Helvica", 24), command=lam
 btn8=Button(mansLogs,text="",width=6, height=3,font=("Helvica", 24), command=lambda:btnClick(btn8))
 btn9=Button(mansLogs,text="",width=6, height=3,font=("Helvica", 24), command=lambda:btnClick(btn9))
 
+#GALVENĀ IZVĒLNE
+galvenaIzvelne=Menu(mansLogs)
+mansLogs.config(menu=galvenaIzvelne)#pievieno galvenajam logam
+
+#MAZĀ IZVĒLNE
+opcijas=Menu(galvenaIzvelne,tearoff=False)#pievieno galvenajam logam
+galvenaIzvelne.add_cascade(label='Opcijas',menu=opcijas)
+opcijas.add_command(label='Jauna spēle',command=reset)
+opcijas.add_command(label='Iziet',command=mansLogs.quit)
+
+
+
+
 btn1.grid(row=0,column=0)#pievieno pogas
 btn2.grid(row=1,column=0)
 btn3.grid(row=2,column=0)
@@ -57,5 +74,39 @@ btn6.grid(row=2,column=1)
 btn7.grid(row=0,column=2)
 btn8.grid(row=1,column=2)
 btn9.grid(row=2,column=2)
+
+#POGU STĀVOKLIS
+def disableButtons():
+    btn1.config(state=DISABLED)
+    btn2.config(state=DISABLED)
+    btn3.config(state=DISABLED)
+    btn4.config(state=DISABLED)
+    btn5.config(state=DISABLED)
+    btn6.config(state=DISABLED)
+    btn7.config(state=DISABLED)
+    btn8.config(state=DISABLED)
+    btn9.config(state=DISABLED)
+    return 0
+
+def reset():
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn1['text']=''
+    btn2['text']=''
+    btn3['text']=''
+    btn4['text']=''
+    btn5['text']=''
+    btn6['text']=''
+    btn7['text']=''
+    btn8['text']=''
+    btn9['text']=''
+
 
 mansLogs.mainloop()
